be96cdef1adcd54e76f300a38f140dae46aa572d
hidenkeys <108880418+hidenkeys@users.noreply.github.com>
2025-08-01 12:37:09 +0000

    new test terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ provider "github" {
 
 # GitHub Repository Resource
 resource "github_repository" "example" {
-  name        = "example-repository-v1"
+  name        = "example-repository-v2"
   description = "A new repository created via Terraform and GitHub Actions"
   visibility  = "public"
 }
@@ -28,4 +28,15 @@ resource "github_repository" "example" {
 output "repository_url" {
   description = "The URL of the created repository"
   value       = github_repository.example.html_url
+}
+
+# Output the repository's name and description
+output "repository_name" {
+  description = "The name of the created repository"
+  value       = github_repository.example.name
+}
+
+output "repository_description" {
+  description = "The description of the created repository"
+  value       = github_repository.example.description
 }
