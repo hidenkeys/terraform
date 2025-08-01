acbac7e44191e9288c1cea5249ee61cdf7fbfd0e
hidenkeys <108880418+hidenkeys@users.noreply.github.com>
2025-08-01 11:32:17 +0000

    terrafrom

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,31 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.6.0"  # Specify the version of the GitHub provider
+    }
+  }
+}
+
+# GitHub Personal Access Token (PAT) - to be passed as an environment variable
 variable "TOKEN" {
   description = "GitHub Personal Access Token"
   type        = string
 }
 
 provider "github" {
-  token = var.TOKEN  # Reference the declared variable
+  token = var.TOKEN  # Use the token from the environment variable
 }
 
+# GitHub Repository Resource
 resource "github_repository" "example" {
-  name        = "example-repository"
+  name        = "example-repository-v1"
   description = "A new repository created via Terraform and GitHub Actions"
   visibility  = "public"
+}
+
+# Output the URL of the created GitHub repository
+output "repository_url" {
+  description = "The URL of the created repository"
+  value       = github_repository.example.html_url
 }
